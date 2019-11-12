adc52056f29621b5b0aa31e6c9cdb83dda294c71
WithuL <1509586319@qq.com>
2019-11-12 22:40:28 +0800

    test5

---
+++ Project5/Project5/work5.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	int a;
+	int b;
+	int c;
+	printf("请输入两个需要交换的数");
+	scanf("%d%d", &a, &b);
+	c = a;
+	a = b;
+	b = c;
+	printf("交换后的数值为：%d  %d", a, b);
+	system("pause");
+	return 0;
+}
