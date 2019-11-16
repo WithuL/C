19a50f9036aa7793e333f46b06b46bec0db3eddb
WithuL <1509586319@qq.com>
2019-11-16 23:41:02 +0800

    实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定，

---
+++ Work6.1/Work6.1/Work.6.1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	int i, j, a;
+	printf("«Î ‰»Î ˝◊÷\n");
+	scanf("%d", &a);
+	for (i = 1; i <= a; i++) {
+		for (j = 1; j <= i; j++) {
+			printf("%d*%d=%d  ", i, j, i*j);
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
