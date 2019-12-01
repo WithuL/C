5a025fe49d5718b959eb1a35dce6953cebfd659e
WithuL <1509586319@qq.com>
2019-12-01 09:33:08 +0800

    在屏幕上打印杨辉三角。 1 1 1 1 2 1 1 3 3 1

---
+++ Work10.2/Work10.2/Work10.2.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	int killer;
+	for (killer = 'a'; killer <= 'd'; killer++) {
+		if(((killer!='a')+(killer=='c')+(killer=='d')+(killer!='d'))==3){
+			printf("%c是凶手！\n", killer);
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
