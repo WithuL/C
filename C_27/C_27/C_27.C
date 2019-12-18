ec483a9bb224d064b44490feb75f10bd1a81b951
WithuL <1509586319@qq.com>
2019-12-18 23:17:11 +0800

    有5个人坐在一起，问第五个人多少岁？他说比第4个人大2岁。问第4个人岁数，他说比第3个人大2岁。问第三个人，又说比第2人大两岁。问第2个人，说比第一个人大两岁。最后问第一个人，他说是10岁。请问第五个人多大？
    程序分析：利用递归的方法，递归分为回推和递推两个阶段。要想知道第五个人岁数，需知道第四人的岁数，依次类推，推到第一人（10岁），再往回推。。

---
+++ C_27/C_27/C_27.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int age(n)
+int n;
+{
+    int c;
+    if (n == 1) c = 10;
+    else c = age(n - 1) + 2;
+    return(c);
+}
+int main()
+{
+    printf("%d\n", age(5));
+    return 0;
+}
